916fea87cf22c582f12d75dd5e6fce25a21dd045
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-27 21:57:45 -0700

    Assignment 3-2

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ def main():
     ########################################
     """
 
+    if num1 <= num2 and num1 <= num3:
+        minval = num1
+        if num2 <= num3:
+            median = num2
+            maxval = num3
+        else:
+            median = num3
+            maxval = num2
+    elif num2 <= num1 and num2 <= num3:
+        minval = num2
+        if num1 <= num3:
+            median = num1
+            maxval = num3
+        else:
+            median = num3
+            maxval = num1
+    else:
+        minval = num3
+        if num1 <= num2:
+            median = num1
+            maxval = num2
+        else:
+            median = num2
+            maxval = num1
+
+
     print(minval, median, maxval)
     ########################################
     # Do not delete the return statement
